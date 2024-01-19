6c66608855a3d3a87580379bdf630a226a6f3e0d
optimus-a1 <154238957+optimus-a1@users.noreply.github.com>
2024-01-19 08:50:44 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
-  difficulty: '0x99999',
+  difficulty: '0x999999',
   tick: 'rBNB',
   walletTablePath: 'wallets.csv',
-  rpcUrl: 'https://binance.nodereal.io',
+  rpcUrl: 'https://bsc.rpc.blxrbdn.com',
 }
 
 module.exports = config
